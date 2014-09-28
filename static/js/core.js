96fd3e56622634197a25998eb71d417db0d0a980
Alberto <cruz.alberto92@gmail.com>
2014-09-28 00:14:38 -0300

    Revert 66c5c11..80112ef
    This rolls back to commit 66c5c11a0cb22de8401c895ea4dd2bfbc819f155.

--- static/js/core.js
+++ static/js/core.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
-		e.preventDefault();
+	    e.preventDefault();
 
-		var target = this.hash,
-		$target = $(target);
+	    var target = this.hash,
+	    $target = $(target);
 
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top
-		}, 900, 'swing', function () {
-			window.location.hash = target;
-		});
-	});
-});
-
-$("Document").ready(function($){
-	var nav = $(".MainMenu");
-	$(window).scroll(function(){
-		if($(this).scrollTop() > 485){
-			nav.addClass("MainMenu-f");
-		} else {
-			nav.removeClass("MainMenu-f");
-		}
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
 	});
 });
